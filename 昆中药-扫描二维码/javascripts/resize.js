437ebb9ada76a24fe04de42d87a109012b667b81
Viriy <541239223@qq.com>
2018-01-03 13:49:20 +0800

    压缩

--- 昆中药-扫描二维码/javascripts/resize.js
+++ 昆中药-扫描二维码/javascripts/resize.js
@@ -6,8 +6,6 @@
         else {
             var user_webset_font=getComputedStyle(doc.documentElement,false)['fontSize'];
         }
-//      取整后与默认16px的比例系数
-        var xs=parseFloat(user_webset_font)/12;
 //      设置rem的js设置的字体大小
         var view_jsset_font,result_font;
         var docEl = doc.documentElement,
@@ -21,9 +19,9 @@
 //              设置rem的js设置的字体大小
                 view_jsset_font=75 * (clientWidth / 750);
 //              最终的字体大小为rem字体/系数
-                result_font=view_jsset_font/xs;
+                //result_font=view_jsset_font/xs;
 //              设置根字体大小
-                docEl.style.fontSize = result_font + 'px';
+                docEl.style.fontSize = view_jsset_font + 'px';
                 }
             else{
                 docEl.style.fontSize = 54 + 'px';
